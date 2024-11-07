77be264b3962ea442341690be85242065b9a83ae
nicolasmm365 <162520612+nicolasmm365@users.noreply.github.com>
2024-11-06 22:22:38 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,45 @@ resource "aws_vpc" "vpc_obligatorio" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 
+}
+
+resource "aws_security_group" "sg_jump_server" {
+  vpc_id = aws_vpc.vpc_obligatorio.id
+  name   = "sg_jump_server"
+
+  # Permitir tráfico SSH (puerto 22) desde cualquier lugar
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Permitir todo el tráfico de salida
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
-    Name = "vpc-obligatorio"
+    Name = "sg_jump_server"
+  }
+}
+
+# 4. Crear una Instancia EC2
+resource "aws_instance" "jump_server" {
+  ami           = "ami-064519b8c76274859"  # AMI de ejemplo para Amazon Linux 2 en us-east-1
+  instance_type = "t2.micro"               # Tipo de instancia (elige según tus necesidades)
+  subnet_id     = aws_subnet.subnet_public_a.id
+  security_groups = [aws_security_group.sg_jump_server.id]  # Asociar el grupo de seguridad
+
+  # Configurar el acceso con clave SSH
+  key_name = "vockey"  # Debes crear la clave en AWS previamente o especificar una existente
+
+  tags = {
+    Name = "jump_server"
   }
 }
 
@@ -27,11 +64,9 @@ resource "aws_security_group" "tf_sg_lb_obligatorio" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # ingresar ip del WAF
   }
-  tags = {
-    Name = "tf_sg_lb_obligatorio"
-  }
+
 }
 
 resource "aws_security_group" "tf_sg_appweb_obligatorio" {
@@ -41,7 +76,7 @@ resource "aws_security_group" "tf_sg_appweb_obligatorio" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # ingresar ip del servidor de administracion
   }
   ingress {
     from_port   = 80
@@ -53,7 +88,7 @@ resource "aws_security_group" "tf_sg_appweb_obligatorio" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # luego de la instalacion se debe quitar la salida a internet (se podria hacer creando un security group con un deny que se aplique luego de que este todo hecho con un depends on)
   }
 
 }
@@ -79,7 +114,7 @@ resource "aws_internet_gateway" "obligatorio_igw" {
 }
 
 # Crear subredes en la zona de disponibilidad A y B dentro de la nueva VPC
-resource "aws_subnet" "subnet_private" {
+resource "aws_subnet" "subnet_private_a" {
   vpc_id            = aws_vpc.vpc_obligatorio.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
@@ -87,7 +122,7 @@ resource "aws_subnet" "subnet_private" {
 
 }
 
-resource "aws_subnet" "subnet_public" {
+resource "aws_subnet" "subnet_private_b" {
   vpc_id            = aws_vpc.vpc_obligatorio.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
@@ -95,11 +130,29 @@ resource "aws_subnet" "subnet_public" {
 
 }
 
-# Crear grupo de subredes para la base de datos dentro de la nueva VPC
-resource "aws_db_subnet_group" "obligatorio_db_subnet_group" {
-  name       = "obligatorio-db-subnet-group"
-  subnet_ids = [aws_subnet.subnet_private.id, aws_subnet.subnet_public.id]
+resource "aws_subnet" "subnet_public_a" {
+  vpc_id            = aws_vpc.vpc_obligatorio.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = "true"
+
 }
+
+resource "aws_subnet" "subnet_public_b" {
+  vpc_id            = aws_vpc.vpc_obligatorio.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = "true"
+
+}
+
+
+
+# Crear grupo de subredes para la base de datos dentro de la nueva VPC
+#resource "aws_db_subnet_group" "obligatorio_db_subnet_group" {
+#  name       = "obligatorio-db-subnet-group"
+#  subnet_ids = [aws_subnet.subnet_private_a.id, aws_subnet.subnet_private_b.id]
+#}
 
 
 resource "aws_route_table" "route_table_obligatorio" {
@@ -112,15 +165,75 @@ resource "aws_route_table" "route_table_obligatorio" {
 
 }
 
-resource "aws_route_table_association" "subnet_private_asso" {
-  subnet_id      = aws_subnet.subnet_private.id
+resource "aws_route_table_association" "subnet_public_a_asso" {
+  subnet_id      = aws_subnet.subnet_public_a.id
   route_table_id = aws_route_table.route_table_obligatorio.id
 }
 
-resource "aws_route_table_association" "subnet_public_asso" {
-  subnet_id      = aws_subnet.subnet_public.id
+resource "aws_route_table_association" "subnet_public_b_asso" {
+  subnet_id      = aws_subnet.subnet_public_b.id
   route_table_id = aws_route_table.route_table_obligatorio.id
 }
+
+# 7. Crear una Elastic IP para el NAT Gateway
+resource "aws_eip" "nat_eip_a" {
+  domain = "vpc"
+}
+
+# 8. Crear el NAT Gateway en la subred pública
+resource "aws_nat_gateway" "nat_gateway_a" {
+  allocation_id = aws_eip.nat_eip_a.id
+  subnet_id     = aws_subnet.subnet_public_a.id
+
+}
+
+# 9. Crear una tabla de rutas para la subred privada
+resource "aws_route_table" "private_route_table_a" {
+  vpc_id = aws_vpc.vpc_obligatorio.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway_a.id
+  }
+
+}
+
+# 10. Asociar la tabla de rutas privada a la subred privada
+resource "aws_route_table_association" "subnet_private_a_asso" {
+  subnet_id      = aws_subnet.subnet_private_a.id
+  route_table_id = aws_route_table.private_route_table_a.id
+}
+
+
+# 7. Crear una Elastic IP para el NAT Gateway
+resource "aws_eip" "nat_eip_b" {
+  domain = "vpc"
+}
+
+# 8. Crear el NAT Gateway en la subred pública
+resource "aws_nat_gateway" "nat_gateway_b" {
+  allocation_id = aws_eip.nat_eip_b.id
+  subnet_id     = aws_subnet.subnet_public_b.id
+
+}
+
+# 9. Crear una tabla de rutas para la subred privada
+resource "aws_route_table" "private_route_table_b" {
+  vpc_id = aws_vpc.vpc_obligatorio.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway_b.id
+  }
+
+}
+
+# 10. Asociar la tabla de rutas privada a la subred privada
+resource "aws_route_table_association" "subnet_private_b_asso" {
+  subnet_id      = aws_subnet.subnet_private_b.id
+  route_table_id = aws_route_table.private_route_table_b.id
+}
+
 
 # Crear un balanceador de carga de aplicación (ALB)
 resource "aws_lb" "obligatorio_alb" {
@@ -128,7 +241,7 @@ resource "aws_lb" "obligatorio_alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.tf_sg_lb_obligatorio.id]
-  subnets            = [aws_subnet.subnet_private.id, aws_subnet.subnet_public.id]
+  subnets            = [aws_subnet.subnet_private_a.id, aws_subnet.subnet_private_b.id]
 
 
 }
@@ -199,41 +312,31 @@ resource "aws_lb_listener_rule" "obligatorio_listener-rule" {
 # }
 
 
-locals {
-  webapp_user_data = <<-EOF
-    #!/bin/bash
-    sudo amazon-linux-extras enable epel
-    sudo yum -y install epel-release
-    sudo yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-    sudo yum-config-manager --enable remi-php54
-    sudo yum -y install php php-cli php-common php-mbstring php-xml php-mysql php-fpm
-    sudo yum -y install httpd git
-    sudo systemctl enable httpd
-    sudo systemctl start httpd
-    sudo yum install -y amazon-efs-utils
-    sudo mount -t efs -o tls ${aws_efs_file_system.efs_obligatorio.id}:/ /var/www/html
-    git clone https://github.com/adandrea8/php-ecommerce   
-    sudo cp -r php-ecommerce/* /var/www/html/
-    sudo sed -i s/db_endpoint/$(echo ${aws_db_instance.obligatorio-db.endpoint} | cut -d: -f1)/g /var/www/html/config.php 
-    sudo yum -y install php-mysql.x86_64
-    sudo yum -y install mariadb.x86_64
-    mysql -h $(echo ${aws_db_instance.obligatorio-db.endpoint} | cut -d: -f1) -u ${aws_db_instance.obligatorio-db.username} -p${aws_db_instance.obligatorio-db.password} ${aws_db_instance.obligatorio-db.db_name} < /var/www/html/dump.sql
-    sudo systemctl restart httpd
-  EOF
-}
+#locals {
+ # webapp_user_data = <<-EOF
+   # #!/bin/bash
+    
+  
+  #  sudo systemctl enable httpd
+  #  sudo systemctl start httpd
+    
+    
+    #sudo systemctl restart httpd
+#  EOF
+#}
 
 resource "aws_launch_template" "webapp_launch_template" {
   name_prefix   = "webapp_launch_template"
-  image_id      = "ami-02aead0a55359d6ec"
+  image_id      = "ami-064519b8c76274859"
   instance_type = "t2.micro"
   key_name      = "vockey"
   ebs_optimized = false  # Opcional: ajusta según tus necesidades
 
-  depends_on = [aws_db_instance.obligatorio-db, aws_efs_file_system.efs_obligatorio]
+  #depends_on = [aws_db_instance.obligatorio-db, aws_efs_file_system.efs_obligatorio]
 
   network_interfaces {
     associate_public_ip_address = true
-    subnet_id                   = aws_subnet.subnet_private.id
+    subnet_id                   = aws_subnet.subnet_private_a.id
     security_groups             = [aws_security_group.tf_sg_appweb_obligatorio.id]
   }
 
@@ -245,7 +348,7 @@ resource "aws_launch_template" "webapp_launch_template" {
     }
   }
 
-  user_data = base64encode(local.webapp_user_data)
+  #user_data = base64encode(local.webapp_user_data)
 }
 
 resource "aws_autoscaling_group" "webapp_autoscaling_group" {
@@ -259,7 +362,7 @@ resource "aws_autoscaling_group" "webapp_autoscaling_group" {
   max_size                  = 3
   desired_capacity          = 2
 
-  vpc_zone_identifier       = [aws_subnet.subnet_private.id, aws_subnet.subnet_public.id]  # ID de la subred donde lanzar las instancias
+  vpc_zone_identifier       = [aws_subnet.subnet_private_a.id, aws_subnet.subnet_private_b.id]  # ID de la subred donde lanzar las instancias
 
   target_group_arns         = [aws_lb_target_group.obligatorio_target_group.arn]  # ARN del Target Group si se usa con un ALB
 
